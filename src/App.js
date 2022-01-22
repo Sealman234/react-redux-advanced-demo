b950715d37a1fde11ab2a9440b4eca033899482e
Sealman <castle2668@gmail.com>
2022-01-22 18:27:25 +0800

    [WIP] Handling Http States & Feedback with Redux

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
+
+import { uiActions } from './store/ui-slice';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
 
   // Re-execute when our cart is changed
   useEffect(() => {
-    fetch('https://react-http-14f5a-default-rtdb.firebaseio.com/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart), // must match 'Content-Type' header
-    })
-      .then((response) => {
-        return response.json();
+    dispatch(
+      uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!',
       })
-      .then((myJson) => {
-        console.log(myJson);
-      });
-  }, [cart]);
+    );
+
+    const sendCartData = async () => {
+      const response = await fetch(
+        'https://react-http-14f5a-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart), // must match 'Content-Type' header
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        })
+      );
+    };
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
+        })
+      );
+    });
+
+    console.log(sendCartData);
+    // Dispatch will never trigger (Redux ensures that)
+  }, [cart, dispatch]);
 
   return (
     <Layout>
