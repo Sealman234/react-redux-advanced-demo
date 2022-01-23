1057809cbf51d34fa54d81f9d41a98b116524397
Sealman <castle2668@gmail.com>
2022-01-23 22:48:54 +0800

    Handling Http States & Feedback with Redux

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-
-import { uiActions } from './store/ui-slice';
+import { Fragment, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { uiActions } from './store/ui-slice';
+
+let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   // Re-execute when our cart is changed
   useEffect(() => {
-    dispatch(
-      uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!',
-      })
-    );
-
     const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
+      );
+
       const response = await fetch(
         'https://react-http-14f5a-default-rtdb.firebaseio.com/cart.json',
         {
@@ -45,6 +47,11 @@ function App() {
       );
     };
 
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
     sendCartData().catch((error) => {
       dispatch(
         uiActions.showNotification({
@@ -60,10 +67,19 @@ function App() {
   }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
